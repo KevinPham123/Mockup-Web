6a69eac6e8ffd053a450523c0482679f943ec99e
KevinPham123 <54450659+KevinPham123@users.noreply.github.com>
2021-07-05 15:00:53 -0500

    Tasks
    Listed tasks to do

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { Button2Component } from './components/button2/button2.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ButtonComponent,
-    Button2Component
+    Button2Component,
+    TasksComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from './Task'
+
+export const TASKS = [
+    {
+        id: 1,
+        text: 'Log in page index page/ html',
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Sign up page index page/ html',
+        day: 'May 6th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Spring Boot for backend',
+        day: 'May 7th at 2:30pm',
+        reminder: false,
+    }
+]
