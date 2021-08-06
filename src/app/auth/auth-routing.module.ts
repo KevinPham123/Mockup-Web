7c633ed6def5af6a5ec5e38fe239fb5c2fb5ce2f
KevinPham123 <54450659+KevinPham123@users.noreply.github.com>
2021-08-06 09:26:07 -0500

    Update auth-routing.module.ts

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ButtonComponent } from '../components/button/button.component';
+import { Button2Component } from '../components/button2/button2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Log in', component: ButtonComponent},
+  { path: 'Sign up', component: Button2Component}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
